541768913e2d61f35a303d90f2e43d6fe633113a
AbdulMuneebSyed <samuneeb786@gmail.com>
2025-06-04 04:42:02 +0530

    Feat: final

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css"
 
 export const metadata: Metadata = {
   title: "MathanGo Project",
-  description: "A Next.js project with Tailwind CSS and TypeScript",,
+  description: "A Next.js project with Tailwind CSS and TypeScript",
 }
 
 export default function RootLayout({
